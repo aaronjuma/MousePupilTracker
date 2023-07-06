963b22094ff5efbae0f4a1d6d1221c7c7d335f0c
Aaron Jumarang <44382744+aaronjuma@users.noreply.github.com>
2023-07-06 15:14:34 -0400

    Imported New Model and Changed Camera Settings

--- Utils/CameraRecord.py
+++ Utils/CameraRecord.py
@@ -1,12 +1,9 @@
 import cv2
 
 cap= cv2.VideoCapture(0)
-
-width= int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
-height= int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
 cap.set(cv2.CAP_PROP_FPS, 30)
-cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
-cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, 320)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 240)
 cap.set(cv2.CAP_PROP_BRIGHTNESS, 0)
 cap.set(cv2.CAP_PROP_CONTRAST, 49)
 cap.set(cv2.CAP_PROP_HUE, 0)
@@ -16,6 +13,8 @@ cap.set(cv2.CAP_PROP_GAMMA, 100)
 cap.set(cv2.CAP_PROP_GAIN, 35)
 cap.set(cv2.CAP_PROP_EXPOSURE, -7)
 
+width= int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+height= int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
 writer= cv2.VideoWriter('Data/basicvideo.mp4', cv2.VideoWriter_fourcc(*"MPJG"), 30.0, (width,height))
 record = False
 
--- main.py
+++ main.py
@@ -25,7 +25,7 @@ def main():
     # DeepLabCut Setup
     dlc_proc = PupilProcessor.PupilProcessor()
     model_path = os.getcwd() + '/DLC_Mice_resnet_50_iteration-1_shuffle-1/'
-    dlc_live = DLCLive(model_path, processor = dlc_proc, display = True, display_radius = 3, resize = 1)
+    dlc_live = DLCLive(model_path, processor = dlc_proc, display = True, display_radius = 3, resize = 0.6)
     dlc_live.init_inference(cam.getFrame())
 
     # Logging Setup
