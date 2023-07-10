51442247b7f7efcd117eb3099b30848c20a90f2f
Aaron Jumarang <44382744+aaronjuma@users.noreply.github.com>
2023-07-10 10:16:59 -0400

    Added extra comments

--- Arduino/PupilActivate/PupilActivate.ino
+++ Arduino/PupilActivate/PupilActivate.ino
@@ -15,48 +15,62 @@ void setup() {
   digitalWrite(13, LOW);
 }
 
+//Logic Variables
 bool signalSent = false;
 unsigned long timeSinceSignal = 0;
 bool potentialHigh = false;
 unsigned long timer = 0;
 float val = 0;
 
-void loop() {
 
+void loop() {
   // Checks if we got a signal from the computer
   if(Serial.available()){
     String incomingByte = Serial.readString();
     val = incomingByte.toFloat();
   }
 
-
+  //Checks if we recently send a signal
   if (signalSent == false){
+
+    //Checks if the current pupil size meets threshold
     if (val >= eyeThreshold){
+
+      //Checks if it is the first time the pupil size hit the threshold
       if (potentialHigh == false){
         timer = millis();
         potentialHigh = true;
       }
+
+      //Checks if the pupil size has already hit the threshold
       else{
         unsigned long timeDiff = (millis() - timer)/1000.0;
+
+        //Checks if the time elapsed since first hitting the threshold has reached the time threshold
         if(timeDiff >= timeThreshold){
           signalSent = true;
-          digitalWrite(13, HIGH);
+          digitalWrite(13, HIGH); //Sends signal (NEEDS TO CHANGE FOR TTL)
           potentialHigh = false;
           timeSinceSignal = millis();
         }
       }
     }
+
+    //Checks if the current pupil size has not met the threshold (resets the timer)
     else{
       potentialHigh = false;
     }
   }
+
+  //If the signal has already been sent
   else{
     unsigned long timeDiff = (millis() - timeSinceSignal)/1000.0;
+
+    //Checks if the the cooldown is good and the system is ready to send a signal again
     if(timeDiff >= cooldown){ //10 seconds
       digitalWrite(13, LOW);
       signalSent = false;
       potentialHigh = false;
     }
   }
-      
 }
